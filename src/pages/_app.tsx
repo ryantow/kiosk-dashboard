696db79e04b347a906b62045a7da50c9990c74ec
Ryan Tow <ryan@studioiowa.com>
2025-08-28 16:27:33 -0500

    adding tailwind styling

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+// src/pages/_app.tsx
+import type { AppProps } from "next/app";
+import "../styles/globals.css";          // <-- make sure this path exists
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
