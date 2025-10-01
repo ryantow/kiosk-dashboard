51885aa9cc81f6e918a0d052b9207a277493ff8b
Ryan Tow <ryan@studioiowa.com>
2025-10-01 07:57:12 -0700

    Adding auth0 to dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/globals.css";          // <-- make sure this path exists
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { UserProvider } from '@auth0/nextjs-auth0/client';  // <--this was added for auth0 support
+import Layout from "@/components/Layout";
+
+export default function App(
+  { Component, pageProps }: AppProps & { pageProps: { user?: any } }
+) {
+  return (
+    <UserProvider user={pageProps.user}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
+  );
 }
