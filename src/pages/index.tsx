2d63257ccc43e67c11c8d3637d5fd2d2c69aa22d
Ryan Tow <ryan@studioiowa.com>
2025-10-01 08:52:42 -0700

    Auth0 fix v12 - tweaks to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { useMemo, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import type { InferGetServerSidePropsType } from "next";
 
 /* -------------------- Types -------------------- */
 type Kiosk = { kiosk_id: string; kiosk_name: string };
