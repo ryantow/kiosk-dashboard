960c138deba42ad50815fa9be8f030f105a1d042
Ryan Tow <ryan@studioiowa.com>
2025-10-01 08:03:28 -0700

    fixed elint and any errors on vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 // src/pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/globals.css";          // <-- make sure this path exists
-import { UserProvider } from '@auth0/nextjs-auth0/client';  // <--this was added for auth0 support
+import type { UserProfile } from '@auth0/nextjs-auth0/client'; // <--this was added for auth0 support
 import Layout from "@/components/Layout";
 
 export default function App(
-  { Component, pageProps }: AppProps & { pageProps: { user?: any } }
+  { Component, pageProps }: AppProps & { pageProps: { user?: UserProfile } }
 ) {
   return (
     <UserProvider user={pageProps.user}>
