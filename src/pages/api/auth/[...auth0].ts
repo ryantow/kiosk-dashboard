603c90645dc249c236a2e0c48601f8e78e04f1db
Ryan Tow <ryan@studioiowa.com>
2025-10-01 08:25:47 -0700

    fixed auth0 v7

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,11 +1,2 @@
-<file name=src/components/AuthButtons.tsx>import { useUser } from "@auth0/nextjs-auth0";
-
-...rest of the file remains unchanged...</file>
-
-<file name=src/pages/index.tsx>import { useUser } from "@auth0/nextjs-auth0";
-
-...rest of the file remains unchanged...</file>
-
-<file name=src/pages/_app.tsx>import { UserProvider } from '@auth0/nextjs-auth0';
-
-...rest of the file remains unchanged...</file>
+import { handleAuth } from '@auth0/nextjs-auth0';
+export default handleAuth();
